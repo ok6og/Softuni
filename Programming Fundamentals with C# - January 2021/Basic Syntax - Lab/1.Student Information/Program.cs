9d6bd8bebe53a5d74e6d5bb45fc45504d4b74b05
Georgi Atanasov <ok6og17@gmail.com>
2021-04-30 20:45:02 +0300

    Organizing the repo
    Organized the repo and added my first problem

---
+++ Programming Fundamentals with C# - January 2021/Basic Syntax - Lab/1.Student Information/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace 1_Student_Information
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            string age = "";
+            string grade = "";
+            for (int i = 0; i < 3; i++)
+            {
+                name = Console.ReadLine();
+                age = Console.ReadLine();
+                grade = Console.ReadLine();
+                Console.Write($"Name: {name}, Age: {age}, Grade: {grade}");
+            }
+        }
+    }
+}
