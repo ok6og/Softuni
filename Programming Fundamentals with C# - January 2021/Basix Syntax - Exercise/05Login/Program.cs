4c867b231a2eb127d3d66ce5ec97f25a09fa2a02
Georgi Atanasov <ok6og17@gmail.com>
2021-05-01 12:47:59 +0300

    05Login

---
+++ Programming Fundamentals with C# - January 2021/Basix Syntax - Exercise/05Login/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = string.Concat(username.Reverse());
+
+            bool isLogged = false;
+            string inputPass = "";
+            int counter = 0;
+
+
+            while (!isLogged)
+            {
+                inputPass = Console.ReadLine();
+                counter++;
+                if (counter == 4)
+                {
+                    if (inputPass != password)
+                    {
+                        Console.WriteLine("Incorrect password. Try again.");
+                    }
+                    else if (inputPass == password)
+                    {
+                        Console.WriteLine($"User {username} logged in.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                }
+
+            }
+        }
+    }
+}
