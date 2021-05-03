c31263772edaaaca241af12337fe149c74987159
Georgi Atanasov <ok6og17@gmail.com>
2021-05-03 19:35:34 +0300

    6th problem

---
+++ Programming Fundamentals with C# - January 2021/Basix Syntax - Exercise/06StrongNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CodeTestProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nCopy = n;
+            int sum = 0;
+
+            while (n > 0)
+            {
+                int factoriel = 1;
+                int number = n % 10;
+                n /= 10;
+
+                for (int i = 2; i <= number; i++)
+                {
+                    factoriel *= i;
+                }
+
+                sum += factoriel;
+            }
+            if (sum == nCopy)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
