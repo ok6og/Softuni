57d11450d65f6a56e430fba26170674dee55bbb3
Georgi Atanasov <ok6og17@gmail.com>
2021-05-06 23:01:33 +0300

    Fixing folders and putting back the problems

---
+++ Programming Fundamentals with C# - January 2021/Data Types and Variables - Lab/02PoundsToDollars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+
+            decimal dollars = pounds * 1.31M;
+
+
+            Console.WriteLine($"{dollars:F3}");
+
+        }
+    }
+}
