9be34ff243eb98071047195c193201585876dafc
Georgi Atanasov <ok6og17@gmail.com>
2021-05-06 23:03:27 +0300

    Centuries to minutes

---
+++ Programming Fundamentals with C# - January 2021/Data Types and Variables - Lab/04CenturiesToMinutes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int centureis = int.Parse(Console.ReadLine());
+            int years = centureis * 100;
+            int days = (int)Math.Truncate(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            Console.WriteLine($"{centureis} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+
+
+        }
+    }
+}
