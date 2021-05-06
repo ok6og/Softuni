bf18f2ae2e1fef16ae978f54592d0ebd2d77dc05
Georgi Atanasov <ok6og17@gmail.com>
2021-05-06 23:02:37 +0300

    RealNum Problem

---
+++ Programming Fundamentals with C# - January 2021/Data Types and Variables - Lab/03ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                sum = sum + num;
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
