2a328138c36761b8d21adb20426b024cd3e7342b
Georgi Atanasov <ok6og17@gmail.com>
2021-05-09 01:52:24 +0300

    09

---
+++ Programming Fundamentals with C# - January 2021/Data Types and Variables - Lab/09/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char char1 = char.Parse(Console.ReadLine());
+            char char2 = char.Parse(Console.ReadLine());
+            char char3 = char.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"{char1}{char2}{char3}");
+        }
+    }
+}
