c1a96191582979957c0b8e3526eff7544ea4aae5
Georgi Atanasov <ok6og17@gmail.com>
2021-05-08 16:38:28 +0300

    07

---
+++ Programming Fundamentals with C# - January 2021/Data Types and Variables - Lab/07. Concat Names/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string char1 = Console.ReadLine();
+            string char2 = Console.ReadLine();
+            string char3 = Console.ReadLine();
+
+            Console.WriteLine($"{char1}{char3}{char2}");
+        }
+    }
+}
