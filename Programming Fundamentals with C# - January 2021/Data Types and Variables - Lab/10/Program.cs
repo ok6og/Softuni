f1a1e60425d65be6443d092aa5c00a2cb1baa703
Georgi Atanasov <ok6og17@gmail.com>
2021-05-09 01:52:52 +0300

    10

---
+++ Programming Fundamentals with C# - January 2021/Data Types and Variables - Lab/10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char lol = char.Parse(Console.ReadLine());
+            bool result = Char.IsUpper(lol);
+
+            if (result)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
