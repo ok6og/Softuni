a2ba7e8728fe8cb4702f80686c66516987d57f13
Georgi Atanasov <ok6og17@gmail.com>
2021-05-09 01:51:46 +0300

    08

---
+++ Programming Fundamentals with C# - January 2021/Data Types and Variables - Lab/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            int population = int.Parse(Console.ReadLine());
+            int area = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+        }
+    }
+}
