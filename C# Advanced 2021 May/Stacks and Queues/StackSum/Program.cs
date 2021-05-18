43b16d4731e14dae5100953b18de230e02917182
Georgi Atanasov <ok6og17@gmail.com>
2021-05-18 20:54:12 +0300

    02 Stack Sum

---
+++ C# Advanced 2021 May/Stacks and Queues/StackSum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
