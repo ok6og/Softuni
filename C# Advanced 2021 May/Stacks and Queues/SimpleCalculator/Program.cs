e7fc1f1bc0787f348af5b54f3b0dd6b2cd8dec23
Georgi Atanasov <ok6og17@gmail.com>
2021-05-18 20:34:13 +0300

    Simple Calculator Fix

--- C# Advanced 2021 May/Stacks and Queues/SimpleCalculator/Program.cs
+++ C# Advanced 2021 May/Stacks and Queues/SimpleCalculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SimpleCalculator
 {
@@ -6,7 +8,30 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+            Stack<string> calculus = new Stack<string>(input);
+
+            while (calculus.Count > 1)
+            {
+                int x = int.Parse(calculus.Pop());
+                string oper = calculus.Pop();
+                int y = int.Parse(calculus.Pop());
+               
+                if(oper == "+")
+                {
+                    calculus.Push((x + y).ToString());
+                }
+                else
+                {
+                    calculus.Push((x - y).ToString());
+                }
+            }
+
+            Console.WriteLine(calculus.Peek());
+
         }
     }
 }
