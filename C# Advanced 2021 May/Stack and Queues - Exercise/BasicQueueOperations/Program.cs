5c6555e7429d005482d868289b6d9b15629c0965
Georgi Atanasov <ok6og17@gmail.com>
2021-05-20 20:25:12 +0300

    fastfood

---
+++ C# Advanced 2021 May/Stack and Queues - Exercise/BasicQueueOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> que = new Queue<int>();
+
+            for (int i = 0; i < commands[0]; i++)
+            {
+                que.Enqueue(numbers[i]);
+            }
+
+            for (int i = 0; i < commands[1]; i++)
+            {
+                que.Dequeue();
+            }
+
+            if (que.Contains(commands[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if(que.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(que.Min());
+            }
+        }
+    }
+}
---
+++ C# Advanced 2021 May/Stack and Queues - Exercise/FastFood/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] ordersQuantity = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> orders = new Queue<int>(ordersQuantity);
+
+            int count = orders.Count();
+
+            Console.WriteLine(orders.Max());
+
+            for (int i = 0; i < count; i++)
+            {
+                if (foodQuantity >= orders.Peek())
+                {
+                    foodQuantity = foodQuantity - orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(' ', orders)}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
