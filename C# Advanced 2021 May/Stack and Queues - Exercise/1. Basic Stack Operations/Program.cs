24f19116ecad7e9af7adf4cb73d2ce732ce72851
Georgi Atanasov <ok6og17@gmail.com>
2021-05-20 19:11:20 +0300

    BasicStackOperations

--- C# Advanced 2021 May/Stack and Queues - Exercise/1. Basic Stack Operations/Program.cs
+++ C# Advanced 2021 May/Stack and Queues - Exercise/1. Basic Stack Operations/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace _1._Basic_Stack_Operations
+namespace BasicStackOperations
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] commands = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] numbers = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < commands[0]; i++)
+            {
+                stack.Push(numbers[i]);
+            }
+
+            for (int i = 0; i < commands[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            if(stack.Contains(commands[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else if(stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
         }
     }
 }
