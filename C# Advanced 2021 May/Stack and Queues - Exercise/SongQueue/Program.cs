fccf5862eef8ad43d6f761901cc80cf9b72ef422
Georgi Atanasov <ok6og17@gmail.com>
2021-05-20 22:10:03 +0300

    Songs

---
+++ C# Advanced 2021 May/Stack and Queues - Exercise/SongQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SongQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Queue<string> songList = new Queue<string>(songs);
+
+            while (songList.Any())
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "Play":
+                        songList.Dequeue();
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songList));
+                        break;
+                    default:
+                        string song = command.Substring(4);
+                        if (songList.Contains(song))
+                        {
+                            Console.WriteLine($"{song} is already contained!");
+                        }
+                        else
+                        {
+                            songList.Enqueue(song);
+                        }
+                        break;
+
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
