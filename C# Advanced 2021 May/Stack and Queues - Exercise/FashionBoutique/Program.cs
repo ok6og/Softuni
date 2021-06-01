3326afc0606c586f17ee724c89d0426d253aac90
Georgi Atanasov <ok6og17@gmail.com>
2021-06-01 19:29:58 +0300

    FashionBoutiqeu

---
+++ C# Advanced 2021 May/Stack and Queues - Exercise/FashionBoutique/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothesBox = new Stack<int>( Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int counter = 1;
+
+            while (clothesBox.Any())
+            {
+                if(sum+clothesBox.Peek() < rackCapacity)
+                {
+                    sum += clothesBox.Pop();
+                }
+                else if(sum+clothesBox.Peek() == rackCapacity)
+                {
+                    clothesBox.Pop();
+                    sum = 0;
+                    counter++;
+                }
+                else if(sum+clothesBox.Peek() > rackCapacity)
+                {
+                    counter++;
+                    sum = 0;
+                    sum += clothesBox.Pop();
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
