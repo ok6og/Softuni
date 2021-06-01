dec1f7d10438194842317a7f021503c437b5d930
Georgi Atanasov <ok6og17@gmail.com>
2021-06-01 20:27:41 +0300

    Para

---
+++ C# Advanced 2021 May/Stack and Queues - Exercise/BalancedPara/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedPara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string para = Console.ReadLine();
+            int half = para.Length / 2;
+
+            Stack<char> paraStack = new Stack<char>(para.Substring(half));
+            Queue<char> paraQueue = new Queue<char>(para.Substring(0, half));
+
+            Console.WriteLine(paraStack.Pop());
+            Console.WriteLine(paraQueue.Dequeue());
+
+            Console.WriteLine(string.Join(" ", paraStack));
+            Console.WriteLine(string.Join(" ", paraQueue));
+
+
+        }
+    }
+}
