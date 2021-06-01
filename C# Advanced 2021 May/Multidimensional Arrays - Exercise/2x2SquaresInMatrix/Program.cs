20f17b704fa298e17315fafa8f6cc6550c2c19f7
Georgi Atanasov <ok6og17@gmail.com>
2021-06-01 21:17:04 +0300

    2x2 Same Matrix

---
+++ C# Advanced 2021 May/Multidimensional Arrays - Exercise/2x2SquaresInMatrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _2x2SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int counter = 0;
+            char[,] matrix = new char[size[0], size[1]];
+
+            for (int row = 0; row < size[0]; row++)
+            {
+                char[] arr = Console.ReadLine()
+                    .Split()
+                    .Select(char.Parse)
+                    .ToArray();
+                for (int col = 0; col < size[1]; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            for (int row = 0; row < size[0]-1; row++)
+            {
+                for (int col = 0; col < size[1]-1; col++)
+                {
+                    if(matrix[row,col] == matrix[row,col+1] &&
+                        matrix[row,col] == matrix[row+1,col]&&
+                        matrix[row,col] == matrix[row+1,col+1])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
