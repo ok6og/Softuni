eea7480c6c8b03d6f742759d4459afeb30bf0a74
Georgi Atanasov <ok6og17@gmail.com>
2021-06-01 21:30:24 +0300

    fixed

--- C# Advanced 2021 May/Multidimensional Arrays - Exercise/2x2SquaresInMatrix/Program.cs
+++ C# Advanced 2021 May/Multidimensional Arrays - Exercise/2x2SquaresInMatrix/Program.cs
@@ -14,21 +14,21 @@ namespace _2x2SquaresInMatrix
             int counter = 0;
             char[,] matrix = new char[size[0], size[1]];
 
-            for (int row = 0; row < size[0]; row++)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
                 char[] arr = Console.ReadLine()
-                    .Split()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                     .Select(char.Parse)
                     .ToArray();
-                for (int col = 0; col < size[1]; col++)
+                for (int col = 0; col < matrix.GetLength(1); col++)
                 {
                     matrix[row, col] = arr[col];
                 }
             }
 
-            for (int row = 0; row < size[0]-1; row++)
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
             {
-                for (int col = 0; col < size[1]-1; col++)
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
                 {
                     if(matrix[row,col] == matrix[row,col+1] &&
                         matrix[row,col] == matrix[row+1,col]&&
