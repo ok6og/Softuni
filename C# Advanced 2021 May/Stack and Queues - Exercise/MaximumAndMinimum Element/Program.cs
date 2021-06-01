031f179ba5e102e21d767360475c7b172c294201
Georgi Atanasov <ok6og17@gmail.com>
2021-06-01 19:09:28 +0300

    MaxMinElement

---
+++ C# Advanced 2021 May/Stack and Queues - Exercise/MaximumAndMinimum Element/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string token1 = "";
+            int token2 = 0;
+
+            Stack<int> elements = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                token1 = Convert.ToString(tokens[0]);
+
+                switch (token1)
+                {
+                    case "1":
+                        elements.Push(tokens[1]);
+                        break;
+                    case "2":
+                        if (elements.Any())
+                        {
+                            elements.Pop();
+                        }
+                        break;
+                    case "3":
+                        if (elements.Any())
+                        {
+                            Console.WriteLine(elements.Max());
+                        }
+                        break;
+                    case "4":
+                        if (elements.Any())
+                        {
+                            Console.WriteLine(elements.Min());
+                        }
+                        break;
+
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", elements));
+ 
+
+        }
+    }
+}
