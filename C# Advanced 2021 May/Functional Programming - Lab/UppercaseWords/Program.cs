9c72c9deeddf8de0d7518c3c250366b8b5e010e5
Georgi Atanasov <ok6og17@gmail.com>
2021-06-02 22:29:40 +0300

    UppercaseWords

---
+++ C# Advanced 2021 May/Functional Programming - Lab/UppercaseWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace UppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> predicate = str => str[0] == str.ToUpper()[0];
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => predicate(x))
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+
+           
+        }
+    }
+}
