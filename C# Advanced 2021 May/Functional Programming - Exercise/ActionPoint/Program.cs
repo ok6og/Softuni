02567c6a7481b91b96a5120b94eabfe69f624d6c
Georgi Atanasov <ok6og17@gmail.com>
2021-06-03 21:08:30 +0300

    ActionPoint

---
+++ C# Advanced 2021 May/Functional Programming - Exercise/ActionPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> name = (n => Console.WriteLine(n));
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(name);
+        }
+    }
+}
