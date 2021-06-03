04755da4f1add3df5bed09a3e4042ca0c42f4f98
Georgi Atanasov <ok6og17@gmail.com>
2021-06-03 23:53:15 +0300

    FindEvenOrOdds

---
+++ C# Advanced 2021 May/Functional Programming - Exercise/FindEvenOrOdds/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindEvenOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           
+
+            int[] tokens = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string oddOrEven = Console.ReadLine();
+
+         
+            var result = new List<int>();
+            Predicate<int> even = oddOrEven == "odd"
+                ? number => number%2!=0
+                : new Predicate<int> (number => number % 2 == 0);
+
+
+
+
+            for (int i = tokens[0]; i <= tokens[1]; i++)
+            {
+                if(even(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+
+        }
+    }
+}
