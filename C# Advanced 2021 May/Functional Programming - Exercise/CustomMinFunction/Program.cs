ca945335bc7110c00e52ab3487d428473653d96b
Georgi Atanasov <ok6og17@gmail.com>
2021-06-03 23:10:50 +0300

    ActionPoint

---
+++ C# Advanced 2021 May/Functional Programming - Exercise/CustomMinFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int[], int> minN = x => x.Min();
+
+            int[] n = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(minN(n));
+        }
+    }
+}
---
+++ C# Advanced 2021 May/Functional Programming - Exercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList()
+                .ForEach(n => Console.WriteLine("Sir " + n));
+        }
+    }
+}
