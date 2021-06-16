d3cb8e10c2439ee4afa7ca466a8e33cec5fc3b93
Georgi Atanasov <ok6og17@gmail.com>
2021-06-16 23:17:28 +0300

    TheWholeLab

---
+++ C# Advanced 2021 May/Defining Classes - Lab/ClassCar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+
+            myCar.Make = "Volkswagen";
+            myCar.Model = "Golf 5";
+            myCar.Year = 2007;
+
+
+        }
+    }
+}
---
+++ C# Advanced 2021 May/Iterators and Comparators - Lab/LibraryManager/Library.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly SortedSet<Book> books;
+
+        public Library(params Book[] _books)
+        {
+            books = new SortedSet<Book>(_books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly IList<Book> books;
+
+            private int currentIndex = -1;
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public LibraryIterator(IEnumerable<Book> _books)
+            {
+                Reset();
+                books = new List<Book>(_books);
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return ++currentIndex < books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+    }
+}
