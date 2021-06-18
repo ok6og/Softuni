9d312319180475f71d45c2a3d92c315873c203ff
Georgi Atanasov <ok6og17@gmail.com>
2021-06-18 14:16:32 +0300

    Froggy

---
+++ C# Advanced 2021 May/Iterators and Comparators - Exercise/Froggy/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
