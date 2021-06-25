cf913dfd2a2e1437ad11531fbebfb751be1c1d42
Georgi Atanasov <ok6og17@gmail.com>
2021-06-25 23:38:32 +0300

    Classroom and Garden

---
+++ C# Advanced 2021 May/cSharp Advanced - Exam Preparation/Classroom/Classroom.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public int Capacity { get; }
+
+        public int Count => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if(Capacity <= Count)
+            {
+                return "No seats in the classroom";
+            }
+            else
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            if (students.Exists(x=> x.FirstName == firstName && x.LastName == lastName))
+            {
+                students.Remove(students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName));
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            var studentsSubject = students.Where(x => x.Subject == subject).ToList();
+            if (studentsSubject.Count>0)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine($"Students:");
+                foreach (Student student in studentsSubject)
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+                return sb.ToString().TrimEnd();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+
+        public int GetStudentsCount() => Count;
+
+        public Student GetStudent(string firstName, string lastName) => students.FirstOrDefault(x => x.FirstName == firstName && x.LastName == lastName);
+
+    }
+}
---
+++ C# Advanced 2021 May/cSharp Advanced - Exam Preparation/Garden/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int isRow = size[0];
+            int isCol = size[1];
+
+            var garden = new int[isRow, isCol];
+
+            while (true)
+            {
+                string tokens = Console.ReadLine();
+                if (tokens =="Bloom Bloom Plow")
+                {
+                    break;
+                }
+
+
+                var row = int.Parse(tokens[0].ToString());
+                var col = int.Parse(tokens[2].ToString());
+
+                if (row < 0 || row > garden.GetLength(0) || col < 0 || col > garden.GetLength(1))
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    continue;
+                }
+
+                for (int i = 0; i < garden.GetLength(0); i++)
+                {
+                    garden[row, i]++;
+                }
+
+                for (int j = 0; j < garden.GetLength(1); j++)
+                {
+                    garden[j, col]++;
+                }
+
+                garden[row, col]--;
+            }
+
+            for (int row = 0; row < isRow; row++)
+            {
+                for (int col = 0; col < isCol; col++)
+                {
+                    Console.Write(garden[row,col]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
