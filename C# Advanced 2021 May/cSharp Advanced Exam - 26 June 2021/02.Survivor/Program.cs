6a8b95ca2dd64fa35072ed4de4c24f6992b39344
Georgi Atanasov <ok6og17@gmail.com>
2021-06-26 11:26:23 +0300

    Second zad

--- C# Advanced 2021 May/cSharp Advanced Exam - 26 June 2021/02.Survivor/Program.cs
+++ C# Advanced 2021 May/cSharp Advanced Exam - 26 June 2021/02.Survivor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.Survivor
 {
@@ -6,7 +8,135 @@ namespace _02.Survivor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int isRows = int.Parse(Console.ReadLine());
+
+            char[][] beach = new char[isRows][];
+
+            for (int row = 0; row < beach.Length; row++)
+            {
+                char[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                beach[row] = input;
+                
+            }
+
+            int myTokenCount = 0;
+            int oppTokenCount = 0;
+
+            while (true)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+
+                if (command == "Gong")
+                {
+                    break;
+                }
+
+
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+                bool isCoordinates = (row >= 0 && row < beach.Length && col >= 0 && col < beach[row].Length);
+                if (tokens[0] == "Find" && isCoordinates)
+                {
+                    if (beach[row][col] == 'T')
+                    {
+                        myTokenCount++;
+                        beach[row][col] = '-';
+                    }
+
+                }
+                else if (tokens[0] == "Opponent" && isCoordinates)
+                {
+                    string move = tokens[3];
+                    if (beach[row][col] == 'T')
+                    {
+                        oppTokenCount++;
+                        beach[row][col] = '-';
+                    }
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (move == "up")
+                        {
+                            row--;
+                            isCoordinates = (row >= 0 && row < beach.GetLength(0) && col >= 0 && col < beach[row].GetLength(0));
+                            if (isCoordinates)
+                            {
+                                if (beach[row][col] == 'T')
+                                {
+                                    oppTokenCount++;
+                                    beach[row][col] = '-';
+                                }
+                            }
+                            else if(!isCoordinates)
+                            {
+                                break;
+                            }
+                        }
+                        else if (move == "down")
+                        {
+                            row++;
+                            isCoordinates = (row >= 0 && row < beach.GetLength(0) && col >= 0 && col < beach[row].GetLength(0));
+                            if (isCoordinates)
+                            {
+                                if (beach[row][col] == 'T')
+                                {
+                                    oppTokenCount++;
+                                    beach[row][col] = '-';
+                                }
+                            }
+                            else if (!isCoordinates)
+                            {
+                                break;
+                            }
+                        }
+                        else if (move == "left")
+                        {
+                            col--;
+                            isCoordinates = (row >= 0 && row < beach.GetLength(0) && col >= 0 && col < beach[row].GetLength(0));
+                            if (isCoordinates)
+                            {
+                                if (beach[row][col] == 'T')
+                                {
+                                    oppTokenCount++;
+                                    beach[row][col] = '-';
+                                }
+                            }
+                            else if (!isCoordinates)
+                            {
+                                break;
+                            }
+                        }
+                        else if (move == "right")
+                        {
+                            col++;
+                            isCoordinates = (row >= 0 && row < beach.GetLength(0) && col >= 0 && col < beach[row].GetLength(0));
+                            if (isCoordinates)
+                            {
+                                if (beach[row][col] == 'T')
+                                {
+                                    oppTokenCount++;
+                                    beach[row][col] = '-';
+                                }
+                            }
+                            else if (!isCoordinates)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            for (int row = 0; row < beach.Length; row++)
+            {                               
+                Console.WriteLine(string.Join(" ", beach[row]));                
+            }
+
+            Console.WriteLine($"Collected tokens: {myTokenCount}");
+
+            Console.WriteLine($"Opponent's tokens: {oppTokenCount}");
         }
     }
 }
