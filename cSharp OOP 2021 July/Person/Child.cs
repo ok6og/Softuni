439a15e533aefa493652ebd52bf871044ac5062d
Georgi Atanasov <ok6og17@gmail.com>
2021-07-11 01:16:15 +0300

    OOP

---
+++ cSharp OOP 2021 July/Person/Child.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    class Child : Person
+    {
+        public Child(string name, int age) 
+            : base(name,age)
+        {
+
+        }
+
+        public override int Age { 
+            get 
+            {
+                return base.Age;
+            }
+            set 
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("fail");
+                }
+                base.Age = value;
+            }
+        }
+
+    }
+}
---
+++ cSharp OOP 2021 July/Person/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+
+        private int age;
+        public string Name { get; set; }
+
+        public virtual int Age 
+        {
+            get 
+            {
+                return this.age;
+            }
+
+            set 
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Age cannot be negative");
+                }
+                this.age = value;
+            }
+        
+        
+        }
+
+        public Person(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
