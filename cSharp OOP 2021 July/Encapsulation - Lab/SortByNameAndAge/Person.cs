3149117c8802295ac7b3cfa9c9268a90a56049e1
Georgi Atanasov <ok6og17@gmail.com>
2021-07-13 19:59:39 +0300

    Encapsulation

---
+++ cSharp OOP 2021 July/Encapsulation - Lab/SortByNameAndAge/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public decimal Salary { get; private set; }
+
+        public Person(string fName, string lName, int age)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = age;
+        }
+
+        public Person(string fName, string lName, int age, decimal salary)
+                :this(fName,lName,age)
+        {
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal increase = percentage;
+            if (Age<30)
+            {
+                increase = percentage / 2;
+            }
+
+            Salary += Salary * increase / 100;
+            
+        }
+    }
+}
