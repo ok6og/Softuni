c690e85f3ec030ddce268af4234c68aed4c114b3
Georgi Atanasov <ok6og17@gmail.com>
2021-07-16 15:32:10 +0300

    ChickenFarm

--- cSharp OOP 2021 July/Encapsulation - Exercise/AnimalFarm/Models/Chicken.cs
+++ cSharp OOP 2021 July/Encapsulation - Exercise/AnimalFarm/Models/Chicken.cs
@@ -1,17 +1,19 @@
-﻿namespace AnimalFarm.Models
+﻿using System;
+
+namespace AnimalFarm.Models
 {
     public class Chicken
     {
-        public const int MinAge = 0;
-        public const int MaxAge = 15;
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
 
-        protected string name;
-        internal int age;
+        private string name;
+        private int age;
 
         internal Chicken(string name, int age)
         {
-            this.name = name;
-            this.age = age;
+            this.Name = name;
+            this.Age = age;
         }
 
         public string Name
@@ -21,8 +23,15 @@
                 return this.name;
             }
 
-            internal set
+            private set
             {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException($"{nameof(Name)} cannot be empty.");
+                }
+	
+
+	
                 this.name = value;
             }
         }
@@ -34,8 +43,16 @@
                 return this.age;
             }
 
-            protected set
+            private set             
             {
+                if (value <= MinAge || value >= MaxAge)
+	            {
+                    throw new ArgumentException($"{nameof(Age)} should be between {MinAge} and {MaxAge}.");
+
+	            }
+	
+
+	
                 this.age = value;
             }
         }
@@ -48,28 +65,14 @@
 			}
         }
 
-        public double CalculateProductPerDay()
+        private double CalculateProductPerDay() => this.Age switch
         {
-            switch (this.Age)
-            {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                    return 1.5;
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                    return 2;
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                    return 1;
-                default:
-                    return 0.75;
-            }
-        }
+            var x when x >= 0 && x < 4 => 1.5,
+            var x when x >= 4 && x < 8 => 2,
+            var x when x >= 8 && x < 12 => 1,
+            _ => 0.75
+        };
+            
+        
     }
 }
--- cSharp OOP 2021 July/Encapsulation - Exercise/AnimalFarm/Program.cs
+++ cSharp OOP 2021 July/Encapsulation - Exercise/AnimalFarm/Program.cs
@@ -9,12 +9,18 @@
             string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
 
-            Chicken chicken = new Chicken(name, age);
-            Console.WriteLine(
-                "Chicken {0} (age {1}) can produce {2} eggs per day.",
-                chicken.Name,
-                chicken.Age,
-                chicken.ProductPerDay);
+            try 
+	        {	        
+		        Chicken chicken = new Chicken(name, age);
+                Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age}) can produce {chicken.ProductPerDay} eggs per day.");
+	        }
+	        catch (Exception ex)
+	        {
+                Console.WriteLine(ex.Message);
+		        
+	        }
+
+          
         }
     }
 }
