85dbfb3255b25c7966d2ebfe9df030607f5a8cb3
Georgi Atanasov <ok6og17@gmail.com>
2021-07-20 16:35:04 +0300

    Cars

---
+++ cSharp OOP 2021 July/Interfaces and Abstraction - Lab/Cars/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Seat seat = new Seat("Leon", "Gray");
+            Tesla tesla = new Tesla(2, "Model 3", "Red");
+
+            Console.WriteLine(seat);
+            Console.WriteLine(tesla);
+        }
+    }
+}
