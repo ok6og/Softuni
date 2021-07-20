30bf151b13e4c94d481e8d95095b23707ac65158
Georgi Atanasov <ok6og17@gmail.com>
2021-07-20 15:57:05 +0300

    Shapes

---
+++ cSharp OOP 2021 July/Interfaces and Abstraction - Lab/Figures/IDrawable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figures
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+}
---
+++ cSharp OOP 2021 July/Interfaces and Abstraction - Lab/Figures/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figures
+{
+    public class Rectangle : IDrawable
+    {
+        private readonly int width;
+        private readonly int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public void Draw()
+        {
+            DrawLine('*', '*');
+            for (int i = 0; i < height-2; i++)
+            {
+                DrawLine('*', ' ');
+            }
+            DrawLine('*', '*');
+        }
+
+        private void DrawLine(char start, char middle)
+        {
+            Console.WriteLine($"{start}{new string(middle, width-2)}{start}");
+        }
+    }
+}
