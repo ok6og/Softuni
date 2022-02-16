1a02d99c502d8b954fc7951cfe2c7790f1552db2
Georgi Atanasov <ok6og17@gmail.com>
2022-02-16 19:52:54 +0200

    DownloadHTMLCode

---
+++ C# Web/AsyncTask/BuildInAsyncMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static async Task Main()
+    {
+        string url = "https://softuni.bg";
+
+        using (HttpClient client = new HttpClient())
+        {
+            var result = await client.GetAsync(url);
+
+            if (result.IsSuccessStatusCode)
+            {
+                string content = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
