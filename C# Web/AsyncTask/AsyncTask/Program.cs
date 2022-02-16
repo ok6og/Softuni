0cb6e79ba0c0e975a2b03f22643166099f18c215
Georgi Atanasov <ok6og17@gmail.com>
2022-02-16 18:54:40 +0200

    AsyncTask

---
+++ C# Web/AsyncTask/AsyncTask/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+Thread thread = new Thread(() =>
+{
+    for (int i = a; i <= b; i++)
+    {
+        if (i%2==0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+});
+thread.Start();
+thread.Join();
+
+
+Console.WriteLine("Thread finished work");
